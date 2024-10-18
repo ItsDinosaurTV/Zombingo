ee4b648aeaa0208ff7e39d3729da60a15f511589
Cameron Tyner <ctyner@webility.ca>
2024-10-18 11:22:02 -0400

    fix: change on:click handler to Svelte 5 syntax

--- src/lib/BingoCard.svelte
+++ src/lib/BingoCard.svelte
@@ -195,7 +195,7 @@
 			<span class="text-lime-500">ZOM</span>BINGO
 			<button
 				class="absolute right-2 top-2 rounded bg-red-500 px-4 py-2 font-bold text-white hover:bg-red-700"
-				on:click={resetCard}
+				onclick={resetCard}
 			>
 				R
 			</button>
